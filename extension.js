62d87b23e07f7a1b6b1c043a14dccf3b90129809
Pedro <pedro@pepicrft.me>
2024-12-08 13:28:12 +0100

    Add a view extension

--- extension.js
+++ extension.js
@@ -25,6 +25,21 @@ function activate(context) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	const launchSimulatorDisposable = vscode.commands.registerCommand('tuist.launchSimulator', function () {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		const panel = vscode.window.createWebviewPanel(
+			'simctlSimulator', // Identifies the type of the webview. Used internally
+			'Simulator', // Title of the panel displayed to the user
+			vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
+			{} // Webview options. More on these later.
+		);
+		panel.webview.html = getWebviewContent();
+	});
+
+	context.subscriptions.push(launchSimulatorDisposable);
 }
 
 // This method is called when your extension is deactivated
@@ -34,3 +49,18 @@ module.exports = {
 	activate,
 	deactivate
 }
+
+
+function getWebviewContent() {
+	return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>Cat Coding</title>
+  </head>
+  <body>
+	  <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+  </body>
+  </html>`;
+  }
